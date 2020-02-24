f6b544c86003613cdc17564aca99e20512d95039
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-24 19:15:36 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -28,6 +28,8 @@ def assembled_matrix
 end
 
 def sorted_matrix
+  organized_storage = []
+  organized_storage = storage_room.sort
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
